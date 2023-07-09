731879df90f4265eefa82ab27f574122cf953962
RichardTCrocker <richardtcrocker@gmail.com>
2023-07-09 19:46:21 -0400

    took out unneeded comments

--- bank_account.py
+++ bank_account.py
@@ -1,34 +1,27 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
     all_accounts = []
     def __init__(self, int_rate, balance = 0): 
         self.balance = balance
         self.int_rate = int_rate
         BankAccount.all_accounts.append(self)
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
         return
     
     def deposit(self, amount):
         self.balance = self.balance + amount
-        # your code here
         return self
     
     def withdraw(self, amount):
         self.balance = self.balance - amount
-        # your code here
         return self
     
     def display_account_info(self):
         print(f"Balance: ${self.balance}")
-        # your code here
         return self
     
     def yield_interest(self):
         yielded_amount = self.balance * self.int_rate
         self.balance = self.balance + yielded_amount
         print(self.balance)
-        # your code here
         return self
     
 
